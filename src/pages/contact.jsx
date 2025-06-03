6aafad2adedaa7e75b252fe7fd225f4d3f151449
ATGBOOM <atgboom@Arnavs-MacBook-Pro.local>
2025-06-03 11:32:22 +0530

    added contact us form

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -6,13 +6,15 @@ function Contact() {
             <h1>Contact Us</h1>
             <p>Need help with something?</p>
             <p>Check out our FAQs or fill out the contact form below with your questions.</p>
-            <form className='contact-form' action="https://formspree.io/f/(PUT FORM ID HERE!!!!)" method="POST">
+            <form className='contact-form' name="contact" method="POST" netlify>
+                <input type="hidden" name="form-name" value="contact" />
+                
                 <div className='names'>
                     <input type="text" name='first_name' placeholder='First Name' required/>
                     <input type="text" name='last_name' placeholder='Last Name' required/>
                 </div>
 
-                <input type="text" name='email' placeholder='Email' required/>
+                <input type="email" name='email' placeholder='Email' required/>
 
                 <div className='message-box'>
                     <textarea name="message" placeholder="Message" rows="7" required></textarea>
