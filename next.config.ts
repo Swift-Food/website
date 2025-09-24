0de6c9d7cdbd005f59b21bec8cdbdb0d9d533d56
Arnav Vaish <arnavvaish@Arnavs-MacBook-Air.local>
2025-09-24 16:21:06 +0100

    added apple pay thing

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/.well-known/:path*',
+        destination: '/.well-known/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
