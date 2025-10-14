6303a3797eb23cee992bf322a483f753499ba487
thadoos <thaddeus.ong.23@ucl.ac.uk>
2025-10-14 18:35:55 +0100

    Temp ignore eslint

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   async headers() {
     return [
       {
-        source: '/.well-known/apple-developer-merchantid-domain-association',
+        source: "/.well-known/apple-developer-merchantid-domain-association",
         headers: [
           {
-            key: 'Content-Type',
-            value: 'text/plain',
+            key: "Content-Type",
+            value: "text/plain",
           },
           {
-            key: 'Cache-Control',
-            value: 'public, max-age=86400',
+            key: "Cache-Control",
+            value: "public, max-age=86400",
           },
         ],
       },
