c6e9e9fb9ddb36eda19e29be94dcd28b31b3119a
thadoos <thaddeus.ong.23@ucl.ac.uk>
2025-10-16 23:20:53 +0100

    Fixed some styling issues

--- app/components/containers/HomeHeroSection.tsx
+++ app/components/containers/HomeHeroSection.tsx
@@ -12,7 +12,7 @@ export default function HomeHeroSection() {
     <div
       className="
       relative rounded-xl overflow-hidden
-      lg:basis-[40%] lg:flex-none
+      lg:basis-[60%] lg:flex-none
       max-lg:w-full max-lg:flex-none max-lg:max-w-md max-lg:mx-auto py-[5%] px-[5%] sm:py-[3%] sm:px-[3%]
     "
     >
@@ -48,16 +48,17 @@ flex flex-col justify-center gap-2 md:gap-4 p-6 max-lg:p-4 max-sm:p-3 max-lg:tex
         className={`text-7xl font-bold text-primary max-lg:text-6xl max-sm:text-4xl  ${styles.montFont}`}
       >
         REAL,
-      </p>
-      <p
-        className={`text-7xl font-bold text-primary max-lg:text-6xl max-sm:text-4xl  ${styles.montFont}`}
-      >
-        LOCAL & FAST
+        <br /> LOCAL & FAST
       </p>
       {/* <p
         className={`text-7xl font-bold text-primary max-lg:text-6xl max-sm:text-4xl  ${styles.montFont}`}
       >
-        FAST
+        LOCAL,
+      </p>
+      <p
+        className={`text-7xl font-bold text-primary max-lg:text-6xl max-sm:text-4xl  ${styles.montFont}`}
+      >
+        & FAST
       </p> */}
       <p className="text-primary leading-relaxed text-xl max-lg:text-base max-sm:text-md font-medium">
         WE ALSO CATER EVENTS OF ANY SIZE - BROWSE THE MENU AND ORDER ONLINE FOR
