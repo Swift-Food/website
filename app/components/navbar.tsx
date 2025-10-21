881a952f2f3e270f5ad758db08db1c39852a7cea
thadoos <thaddeus.ong.23@ucl.ac.uk>
2025-10-21 23:04:37 +0100

    Fixed navbar issue when text wraps

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -36,7 +36,9 @@ function NavbarAction({ onLinkClick }: { onLinkClick?: () => void }) {
 }
 export default function Navbar() {
   const closeDrawer = () => {
-    const drawerCheckbox = document.getElementById("my-drawer") as HTMLInputElement;
+    const drawerCheckbox = document.getElementById(
+      "my-drawer"
+    ) as HTMLInputElement;
     if (drawerCheckbox) {
       drawerCheckbox.checked = false;
     }
@@ -44,12 +46,12 @@ export default function Navbar() {
 
   return (
     <nav className="sticky top-0 left-0 right-0 flex flex-col z-50">
-      <div className="flex items-center justify-between px-16 py-4 max-lg:px-4 max-lg:items-start bg-secondary gap-5">
-        <div className="hidden sm:block w-full max-w-xs">
+      <div className="flex items-center justify-between px-16 py-4 max-lg:px-4 max-lg:items-start bg-secondary gap-5 flex-nowrap">
+        <div className="hidden md:block w-full max-w-xs">
           {/* <SearchBar /> */}
         </div>
         <Link href={"/"} className="cursor-pointer">
-          <div className="flex items-center gap-4 cursor-pointer h-full">
+          <div className="flex items-center gap-4 cursor-pointer h-full whitespace-nowrap">
             {/* <Image
             src="/logo.png"
             width={40}
@@ -57,16 +59,16 @@ export default function Navbar() {
             alt="swift foods logo"
           /> */}
             <label
-              className={`font-bold text-primary text-5xl cursor-pointer ${styles.montFont}`}
+              className={`font-bold text-primary text-5xl cursor-pointer ${styles.montFont} whitespace-nowrap`}
             >
               SWIFT FOOD
             </label>
           </div>
         </Link>
-        <div className="visible max-sm:hidden">
+        <div className="visible max-md:hidden whitespace-nowrap">
           <NavbarAction />
         </div>
-        <div className="drawer w-fit hidden max-sm:block">
+        <div className="drawer w-fit hidden max-md:block">
           <input id="my-drawer" type="checkbox" className="drawer-toggle" />
           <div className="drawer-content">
             <label className="btn btn-square drawer-button" htmlFor="my-drawer">
