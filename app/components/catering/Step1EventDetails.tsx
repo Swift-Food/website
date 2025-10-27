51a8f5c52ef8fed2a1a9f1b8fa72eec7baa3e5ce
thadoos <thaddeus.ong.23@ucl.ac.uk>
2025-10-27 20:19:30 +0000

    Added optional organization entry; some styling tweaks for consistency of submit buttons

--- app/components/catering/Step1EventDetails.tsx
+++ app/components/catering/Step1EventDetails.tsx
@@ -118,6 +118,7 @@ export default function Step1EventDetails() {
 
   const [addressFormData, setAddressFormData] = useState<ContactInfo>(
     contactInfo || {
+      organization: "",
       fullName: "",
       email: "",
       phone: "",
