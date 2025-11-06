a7777d29d88b680be0e7271ef892e3c6f0e66eb7
thadoos <thaddeus.ong.23@ucl.ac.uk>
2025-11-06 15:29:25 +0000

    removed the max length from description

--- app/restaurant/menu/[restaurantId]/edit/[itemId]/page.tsx
+++ app/restaurant/menu/[restaurantId]/edit/[itemId]/page.tsx
@@ -413,7 +413,7 @@ const EditMenuItemPage = () => {
               <textarea
                 value={description}
                 onChange={(e) => setDescription(e.target.value)}
-                maxLength={200}
+                // maxLength={200}
                 rows={3}
                 className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent text-gray-900"
               />
