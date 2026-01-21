d5a468755ac439e86241be572d787f563c3703ac
thadoos <thaddeus.ong.23@ucl.ac.uk>
2026-01-21 17:39:20 +0000

    Restyled contact page

--- app/(support)/contact/page.tsx
+++ app/(support)/contact/page.tsx
@@ -3,9 +3,5 @@
 import ContactForm from "@/lib/components/contact_form";
 
 export default function ContactUsPage() {
-  return (
-    <div className="mt-10">
-      <ContactForm />
-    </div>
-  );
+  return <ContactForm />;
 }
