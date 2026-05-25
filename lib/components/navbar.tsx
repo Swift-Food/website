1e90ba36e607ffcad3119e1bfff5f46793a01547
thadoos <thaddeus.ong.23@ucl.ac.uk>
2026-05-25 15:57:02 +0100

    Added b2b link on navbar

--- lib/components/navbar.tsx
+++ lib/components/navbar.tsx
@@ -66,6 +66,12 @@ export default function Navbar() {
             >
               Contact Us
             </Link>
+            <Link
+              href="/home-v2#business"
+              className="hover:text-[#fa43ad] transition-colors uppercase"
+            >
+              For Business
+            </Link>
             <Link href="/event-order">
               <button className="bg-[#fa43ad] text-white rounded-xl px-6 py-2.5  hover:bg-[#e03a9a] transition-all duration-300 active:scale-95 shadow-lg shadow-black/5 uppercase text-xs font-bold tracking-widest">
                 Order Now
@@ -168,6 +174,13 @@ export default function Navbar() {
               onClick={closeMobileMenu}
             >
               Event Ordering
+            </Link>
+            <Link
+              href="/home-v2#business"
+              className="text-lg font-medium tracking-wide hover:text-[#fa43ad] transition-colors"
+              onClick={closeMobileMenu}
+            >
+              For Business
             </Link>
           </div>
 
